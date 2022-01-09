d7af745cf40ada4d73e4c8e3d1e234c240b79906
Roberto de Lima Souza <robertodelimasouza@gmail.com>
2022-01-09 14:20:48 -0300

    Selection sort

---
+++ SelectionSort/SelectionSort.go
@@ -0,0 +1,61 @@
+package main
+
+import "math/rand"
+
+func main() {
+
+	const arraySize = 5
+
+	var arrayInt [arraySize]int
+
+	for element := 0; element < arraySize; element++ {
+
+		arrayInt[element] = rand.Intn(arraySize)
+	}
+	println("Random Array")
+	for i := 0; i < len(arrayInt); i++ {
+		println(arrayInt[i])
+	}
+	println("---")
+	var sortedArray = SelectionSort(arrayInt[:])
+
+	println("Sorted Array")
+	for i := 0; i < len(sortedArray); i++ {
+		println(sortedArray[i])
+	}
+
+}
+
+func findSmallestInt(sortArray []int) int {
+
+	var smallest = sortArray[0]
+	var smallest_index = 0
+
+	for i := 0; i < len(sortArray); i++ {
+		if sortArray[i] < smallest {
+			smallest = sortArray[i]
+			smallest_index = i
+		}
+	}
+
+	return smallest_index
+}
+
+func removeAtIndex(array []int, index int) []int {
+	//moves the last position to target index
+	array[index] = array[len(array)-1]
+	//returns slice without last position
+	return array[:len(array)-1]
+}
+
+func SelectionSort(sortArray []int) []int {
+	var sortedArray []int
+	var length = len(sortArray)
+	for i := 0; i < length; i++ {
+		var smallest = findSmallestInt(sortArray)
+		sortedArray = append(sortedArray, sortArray[smallest])
+		sortArray = removeAtIndex(sortArray, smallest)
+	}
+
+	return sortedArray
+}
